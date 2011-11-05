bd0ee08bac55d213c6e1be73e38d9aef52e3ec41
Hugh Cole-Baker <sigmaris@gmail.com>
2011-11-05 00:37:14 +0000

    Scroll graph to commit when loading it.
    Make sure that displayed commits are always visible in the graphview.

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,28 +1,35 @@
 function highlight(sha) {
-  var jQ = (window.parent ? window.parent.jQuery : window.jQuery);
-  jQ('.label').attr('fill','black');
-  jQ('#label_'+sha+', #author_'+sha+', #date_'+sha).attr('fill','blue');
+  $('.label').attr('fill','black');
+  $('#label_'+sha+', #author_'+sha+', #date_'+sha).attr('fill','blue');
 }
 
 function scrollToCommit(sha) {
-  var jQ = (window.parent ? window.parent.jQuery : window.jQuery);
-  var labelOnPage = jQ('#'+sha);
-  if(labelOnPage.length > 0) {
+  var labelOnPage = $('#'+sha);
+  if(
+    labelOnPage.length > 0 &&
+    (
+      labelOnPage.position().top < 30 ||
+      labelOnPage.position().top > ($('#top_graph').height() - 30)
+    )
+  ) {
     //Reveal commit on page
-    jQ('#top_graph').animate({ scrollTop: labelOnPage.position().top });
+    $('#top_graph').animate({
+      scrollTop: labelOnPage.position().top + 
+        $('#top_graph').scrollTop() -
+        $('#top_graph').height() / 2
+    });
   }
 }
 
 function searchForCommit(sha) {
-  var jQ = (window.parent ? window.parent.jQuery : window.jQuery);
-  var labelOnPage = jQ('#'+sha);
+  var labelOnPage = $('#'+sha);
   if(labelOnPage.length > 0) {
     //Reveal commit on page
     scrollToCommit(sha)
     load_commit(sha);
   } else {
     //Try searching on the server in the rest of the branch
-    jQ('#reveal_ajax').load(getHeadGraphURL(), jQ.param({
+    $('#reveal_ajax').load(getHeadGraphURL(), $.param({
       offset: gitgraph.loaded_count,
       branches: gitgraph.existing_branches,
       search_commit: sha
@@ -31,15 +38,15 @@ function searchForCommit(sha) {
 }
 
 function load_commit(sha) {
-  var jQ = (window.parent ? window.parent.jQuery : window.jQuery);
   if(gitgraph.loaderTimeout) {
     if(console) {
       console.log("Busy loading another commit...");
     }
   } else {
-    jQ('#bottom_pane').load('/sha/' + sha, '', function(responseText, textStatus, jqXHR) {
+    $('#bottom_pane').load('/sha/' + sha, '', function(responseText, textStatus, jqXHR) {
       if(textStatus == 'success' || textStatus == 'notmodified') {
         highlight(sha);
+        scrollToCommit(sha);
         if(gitgraph.top_expanded) {
           toggleTopExpansion();
         }
@@ -48,12 +55,11 @@ function load_commit(sha) {
   }
 }
 
-var jQ = (window.parent ? window.parent.jQuery : window.jQuery);
-jQ(document).ready(function() {
+$(document).ready(function() {
   
-  jQ('.label').live('click', function(event) {
+  $('.label').live('click', function(event) {
     var elementId = $(event.target).attr('id');
-    load_commit(elementId.substring(elementId.indexOf('_') + 1));
+    searchForCommit(elementId.substring(elementId.indexOf('_') + 1));
     
   }).live('mouseover', function(inEvent) {
     $(inEvent.target).css('text-decoration','underline');
@@ -62,8 +68,8 @@ jQ(document).ready(function() {
     $(outEvent.target).css('text-decoration','');
   });
   
-  jQ('.node').live('click', function(event) {
-    load_commit($(event.target).attr('id'));
+  $('.node').live('click', function(event) {
+    searchForCommit($(event.target).attr('id'));
   });
   
 });
